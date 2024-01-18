d4527dfb5c707bfc4c175a979c2c04c734439b88
lenovo <lenovo@lenovo-PC-8>
2024-01-18 13:40:51 +0530

    1st comment

---
+++ IntroToJava/src/com/edubridge/compileinput/CompileInputDemo.java
@@ -0,0 +1,22 @@
+package com.edubridge.compileinput;
+// 
+public class CompileInputDemo {
+
+	public static void main(String[] args) {
+		byte num1 = 127;
+		short num2 = 3433;
+		int num3 = 354656564;
+		long num4= 544545454;
+		char c= 'X';
+		String str ="edubridge";
+		float num5=32.6f;
+		double num6 = 78.9;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(c);
+				
+	}
+
+}
